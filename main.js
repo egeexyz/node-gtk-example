bbd516374d52147d9a1854cd1799e477ebad9af8
egee-irl <brian@egee.io>
2018-09-30 05:47:24 -0700

    a bit more

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const gi = require('node-gtk');
+const gi = require("node-gtk");
 
 const Gtk = gi.require("Gtk", "3.0");
 
 gi.startLoop();
 Gtk.init();
+
+const header = new Gtk.HeaderBar();
+header.setTitle("Node-GTK");
+header.setSubtitle("A test for node-gtk")
+header.setShowCloseButton(true);
