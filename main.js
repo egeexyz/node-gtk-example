caf3d93f957b76b1c15ac9c1ec459022d61bddc7
egee-irl <brian@egee.io>
2018-10-15 21:17:02 -0700

    rewrote example

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ gi.startLoop();
 Gtk.init();
 
 const header = new Gtk.HeaderBar();
-header.setTitle("Node-GTK");
-header.setSubtitle("A test for node-gtk")
+const win = new Gtk.Window();
+
+header.setTitle("Test!");
+header.setSubtitle("Subtitle test");
 header.setShowCloseButton(true);
+
+win.on("destroy", () => Gtk.mainQuit());
+win.on("delete-event", () => false);
+
+win.setDefaultSize(350, 70);
+win.setTitlebar(header);
+win.borderWidth = 10;
+
+win.setDefaultSize(200, 80);
+// win.add(new Gtk.Button.withLabel("Hello Vala"));
+win.add(new Gtk.Label({ label: "Hello Gtk+" }));
+
+win.showAll();
+Gtk.main();
