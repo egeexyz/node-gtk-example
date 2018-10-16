b0dea27cd97455c2c09e3de3edd8da6b72b4da6a
egee-irl <brian@egee.io>
2018-10-15 21:31:11 -0700

    rewite again to match gtk-languages

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 const gi = require("node-gtk");
 
 const Gtk = gi.require("Gtk", "3.0");
-
 gi.startLoop();
 Gtk.init();
 
 const header = new Gtk.HeaderBar();
-const win = new Gtk.Window();
-
-header.setTitle("Test!");
-header.setSubtitle("Subtitle test");
+header.setTitle("Node-GTK");
+header.setSubtitle("An app made with Node-GTK!");
 header.setShowCloseButton(true);
 
-win.on("destroy", () => Gtk.mainQuit());
-win.on("delete-event", () => false);
+const window = new Gtk.Window();
+window.windowPosition = Gtk.WindowPosition.CENTER;
+window.on("destroy", () => Gtk.mainQuit());
+window.on("delete-event", () => false);
+window.setDefaultSize(350, 70);
+window.setTitlebar(header);
+window.borderWidth = 10;
 
-win.setDefaultSize(350, 70);
-win.setTitlebar(header);
-win.borderWidth = 10;
+window.add(new Gtk.Button({ label: "Hello Node-GTK" }));
+window.showAll();
 
-win.setDefaultSize(200, 80);
-// win.add(new Gtk.Button.withLabel("Hello Vala"));
-win.add(new Gtk.Label({ label: "Hello Gtk+" }));
-
-win.showAll();
 Gtk.main();
